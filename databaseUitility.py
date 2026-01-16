31e67b19525ada589498cec501bb2e5494291e42
Fabianjsz <fabian.juszkiewicz@proton.me>
2026-01-16 22:07:27 +0100

    implementet table setup for kundenTable and ausweisTable

--- databaseUitility.py
+++ databaseUitility.py
@@ -5,14 +5,40 @@ import os
 #Variables
 global dbName
 
-dbName = "Hintergrundsystem"
+dbName = "Hintergrundsystem.db"
 
 
 #Functions
 
-def createTable():
+def createTable(table):
     con = sqlite3.connect(dbName)
     cursor = con.cursor()
-    cursor.execute("")
+    cursor.execute(table)
     con.commit()
     con.close()
+
+
+kundenTable = """CREATE TABLE IF NOT EXISTS kunden(
+    nr INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    vorname TEXT,
+    geschlecht VARCHAR(1),
+    geburtsdatum DATE,
+    strasse TEXT,
+    plz VARCHAR(6),
+    ort TEXT,
+    ausweisart_id INTEGER,
+    ausweisnummer TEXT,
+    gesetzlicher_vertreter_id INTEGER,
+    FOREIGN KEY (ausweisart_id) REFERENCES ausweisart(id),
+    FOREIGN KEY (gesetzlicher_vertreter_id) REFERENCES kunden(nr)
+)"""
+
+ausweisTable = """CREATE TABLE IF NOT EXISTS ausweisart(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bezeichnung TEXT
+    )"""
+
+
+
+createTable()
